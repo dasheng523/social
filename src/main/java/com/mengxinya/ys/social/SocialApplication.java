1f0b23396f57dd256fb25a23449de54384261008
yesheng <dasheng523@163.com>
2022-04-06 23:37:10 +0800

    init

---
+++ src/main/java/com/mengxinya/ys/social/SocialApplication.java
@@ -0,0 +1,32 @@
+package com.mengxinya.ys.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SocialApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SocialApplication.class, args);
+    }
+
+    @RestController
+    static class EchoController {
+
+        @GetMapping("/echo/{string}")
+        public String echo(@PathVariable String string) {
+            return "hello Nacos Discovery " + string;
+        }
+
+        @GetMapping("/divide")
+        public String divide(@RequestParam Integer a, @RequestParam Integer b) {
+            return String.valueOf(a / b);
+        }
+
+    }
+}
